7dd13dbbb5392baf965638603a2f1c235f7dd929
Maksym909 <dolgovmaxim5@gmail.com>
2023-05-08 21:02:24 +0300

    Add files via upload

---
+++ ConsoleApp1/HolyHedgehogs.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Hedgehog
+    {
+        #region
+        enum Color : int
+        {
+            Red = 0,
+            Green = 1,
+            Blue = 2
+        }
+        // 0 - червоний, 1 - зелений, 2 - синій.
+        private int CheckColor(int requireColor)
+        {
+            int last = (int)Enum.GetValues(typeof(Color)).Cast<Color>().Last();
+            int first =(int) Enum.GetValues(typeof(Color)).Cast<Color>().First();
+            if (requireColor < first && requireColor > last)
+                return -1;
+            else
+                return 0;
+        }
+        private int CheckAmount(int requireColor, int[] hedgehogs)
+        {
+            if (hedgehogs[requireColor] == hedgehogs.Sum())
+                return -1;
+            else 
+                return 0;
+        }
+        private int CheckNegativeNumb(int[] hedgehogs)
+        {
+            for (int i = 0; i < hedgehogs.Length; i++)
+            {
+                if (hedgehogs[i] < 0 || hedgehogs[i] > int.MaxValue)
+                    return -1;
+            }
+
+            return 0;
+        }
+        private int CheckAll(int requireColor, int[] hedgehogs)
+        {
+            int color = CheckColor(requireColor);
+            int negativeNumb = CheckNegativeNumb(hedgehogs);
+            int amount = CheckAmount(requireColor, hedgehogs);
+            
+            bool result = color + negativeNumb + amount == 0 ? true : false;
+            if (result)
+                return 0;
+            else
+                return -1;
+
+        }
+
+        private  int Logic(int requireColor, int[] hedgehogs)
+        {
+            int allHedgehogs = hedgehogs.Sum();
+            int require = hedgehogs[requireColor];
+            int counter = 0;
+            int tempColor;
+
+            do{
+                counter++;
+                if (allHedgehogs - hedgehogs.Max()-hedgehogs.Min() == allHedgehogs / hedgehogs.Length) // correct count if all colors equal 
+                {
+                    return counter;
+                }
+
+                tempColor = hedgehogs.ToList().IndexOf(hedgehogs.Max());  
+                for (int i = 0; i < hedgehogs.Length; i++)
+                {
+                    if (i != tempColor)
+                    {
+                        hedgehogs[i] += hedgehogs[tempColor];
+                        hedgehogs[tempColor] = 0;
+                    }
+                }
+                allHedgehogs -= hedgehogs[tempColor];
+                require -= hedgehogs[tempColor];
+                hedgehogs[tempColor] = 0;
+                
+
+                if (require == allHedgehogs)
+                {
+                    return counter;
+                }
+
+                if (require == 0 || allHedgehogs - require < 2)
+                {
+                    return -1;
+                }
+              
+            } while (tempColor != requireColor);
+
+
+            return counter;
+        }
+        #endregion
+        public int ChangeColor(int requireColor, int[] hedgehogs)
+        {
+            if (CheckAll(requireColor, hedgehogs) == 0)
+                return Logic(requireColor, hedgehogs);
+            else
+                return -1;
+        }
+
+    }
+    public class HolyHedgehogs
+    {
+       
+        public static void Main()
+        {
+            int[] list = new[] { 8,1,9 }; //3
+            int[] list1 = new[] { 0, 0, 10 }; //-1
+            int[] list2 = new[] { 15, 15, 15 }; //1
+            int[] list3 = new[] { -1, 4, 85 };//-1
+            int[] list4 = new[] {1001, 1001, 1001 };
+           
+
+            Hedgehog hedge = new Hedgehog();
+
+            Console.WriteLine(hedge.ChangeColor(1, list)); //3
+            Console.WriteLine(hedge.ChangeColor(2, list1));//-1
+            Console.WriteLine(hedge.ChangeColor(0, list2));//1
+
+            Console.WriteLine(hedge.ChangeColor(1, list3));//-1
+            Console.WriteLine(hedge.ChangeColor(2, list4));//1
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/SudokuValidation.js
@@ -0,0 +1,80 @@
+﻿// did you take this task from codeWars ?
+//row 
+function validRow(board, row, col, value) {
+    // j column
+    for (let j = 0; j < 8; j++) {
+        if (j !== col) {
+            if (board[row][j] === value) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+//column 
+function validColumn(board, row, col, value) {
+    // j row
+    for (let i = 0; i < 8; i++) {
+        if (i !== row) {
+            if (board[i][col] === value) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+//The sub-boxes function.
+function validBox(board, row, col, value) {
+    const startRow = row - (row % 3), startCol = col - (col % 3);
+
+    for (let i = startRow; i < startRow + 3; i++) {
+        for (let j = startCol; j < startCol + 3; j++) {
+            if (i !== row && j !== col) {
+                if (board[i][j] === value) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+}
+function validSudoku(board) {
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            const value = board[i][j];
+            if (value !== '.') {
+                if (!validRow(board, i, j, value) || !validColumn(board, i, j, value) | !validBox(board, i, j, value)) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+};
+console.log(validSudoku([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+]));
+console.log(validSudoku([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 0, 3, 4, 8],
+    [1, 0, 0, 3, 4, 2, 5, 6, 0],
+    [8, 5, 9, 7, 6, 1, 0, 2, 0],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 0, 1, 5, 3, 7, 2, 1, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 0, 0, 4, 8, 1, 1, 7, 9]
+]));
